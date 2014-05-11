b09cb1ea4498c4b82bd5b44848ec443353b541bc
Kat Chuang <katychuang@gmail.com>
2014-05-10 21:22:49 -0400

    add examples in base settings file

--- example/base_settings_example.py
+++ example/base_settings_example.py
@@ -1,2 +1,26 @@
+# See formats below:
+
     api_key = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
     api_secret = 'YYYYYYYYYYYYYYYY'
+
+
+# The followng are used in views.py
+
+FLICKR_API_KEY = ''
+FLICKR_API_SECRET = ''
+
+INSTAGRAM_CLIENT_ID = ''
+INSTAGRAM_CLIENT_SECRET = ''
+
+NYTIMES_API_KEY = ""
+
+PEARSON_API_KEY = ""
+
+TUMBLR_API_KEY = ""
+
+TWITTER_CONSUMER_KEY = ''
+TWITTER_CONSUMER_SECRET = ''
+
+TWITTER_KEY = ''
+TWITTER_SECRET = ''
+
